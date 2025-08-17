3599ea8f48d6cde93b7c8386499dd189d17d3ff3
Bhupeshi <bhupesh.dhapola@gmail.com>
2025-08-17 15:48:35 +0530

    Clean commit without large binaries

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+# resource "aws_instance" "web" {
+#   ami           = "ami-0f58b397bc5c1f2e8"
+#   instance_type = "t2.nano"
+#   key_name      = "aws-key"
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ssm_parameter.amazon_linux.value
+  instance_type = "t2.micro"
+  key_name      = "aws-key"
+    tags = {
+        Name = "HelloWorld"
+    }
+}
---
+++ terraform-s3/s3.tf
@@ -0,0 +1,7 @@
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "dhapola-terraform-demo-bucket"
+#   tags = {
+#     Name        = "My Terraform Demo Bucket"
+#     Environment = "Development"
+#   }
+# }
