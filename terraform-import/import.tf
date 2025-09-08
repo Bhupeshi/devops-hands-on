7862dab21002ef3411e53d33ec73588a936c1a25
Bhupeshi <bhupesh.dhapola@gmail.com>
2025-09-08 20:49:33 +0530

    changed instance type to check

--- terraform-import/import.tf
+++ terraform-import/import.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   ami           = "ami-00ca32bbc84273381"
   instance_type = "t3.micro"
   tags = {
-    Name = "Import_EC2"
+    Name = "test"
   }
   key_name = "aws-key"
 }
